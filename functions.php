5b8975c7437c356f2519fb9f54ebee3a2fd78e76
Vinicius Costa <vinicius.costa@urano.pro>
2025-03-19 16:51:41 -0300

    Configs

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
-require_once ABSPATH . "/wp-content/themes/Template-blocks/vendor/autoload.php";
-require_once ABSPATH . "/wp-content/themes/Template-blocks/inc/setup.php";
-require_once ABSPATH . "/wp-content/themes/Template-blocks/inc/blocks.php";
-require_once ABSPATH . "/wp-content/themes/Template-blocks/inc/common.php";
+require_once ABSPATH . "/wp-content/themes/dicina/vendor/autoload.php";
+require_once ABSPATH . "/wp-content/themes/dicina/inc/setup.php";
+require_once ABSPATH . "/wp-content/themes/dicina/inc/blocks.php";
+require_once ABSPATH . "/wp-content/themes/dicina/inc/common.php";
 
 // Functions to load more posts
-require_once ABSPATH . "/wp-content/themes/Template-blocks/inc/requests/load_more.php";
+require_once ABSPATH . "/wp-content/themes/dicina/inc/requests/load_more.php";
 
-require_once ABSPATH . "/wp-content/themes/template-blocks/inc/custom-post-type/vagas_custom_post_type.php";
-require_once ABSPATH . "/wp-content/themes/template-blocks/inc/custom-post-type/candidate_contact.php";
+require_once ABSPATH . "/wp-content/themes/dicina/inc/custom-post-type/vagas_custom_post_type.php";
+require_once ABSPATH . "/wp-content/themes/dicina/inc/custom-post-type/candidate_contact.php";
